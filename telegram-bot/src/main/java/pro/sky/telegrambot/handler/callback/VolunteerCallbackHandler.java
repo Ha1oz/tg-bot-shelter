3c1ee871e3d34b903d764d6a7d172d5fffccfc86
d1mk0v <79874763588@ya.ru>
2023-09-09 17:52:40 +0500

    Добавлен JavaDoc

--- telegram-bot/src/main/java/pro/sky/telegrambot/handler/callback/VolunteerCallbackHandler.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/handler/callback/VolunteerCallbackHandler.java
@@ -2,17 +2,32 @@ package pro.sky.telegrambot.handler.callback;
 
 import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
-import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.EditMessageText;
 
+/**
+ * Обработчик для колбэков связанных с волонтерством.
+ */
 public class VolunteerCallbackHandler implements CallbackChainHandler{
+
+    /**
+     * Проверяет, соответствует ли колбэк необходимым условиям обработчика волонтерства.
+     *
+     * @param update Обновление от Telegram API
+     * @return true, если колбэк соответствует условиям обработчика волонтерства, иначе false.
+     */
     @Override
     public boolean check(Update update) {
         CallbackQuery callbackQuery = update.callbackQuery();
         return callbackQuery != null && callbackQuery.data().equals("volunteer");
     }
 
+    /**
+     * Обрабатывает колбэк связанный с волонтерством и возвращает EditMessageText объект с обновленным текстом сообщения
+     * или null, если колбэк не обрабатывается обработчиком.
+     *
+     * @param update Обновление от Telegram API
+     * @return EditMessageText объект с обновленным текстом сообщения или null, если колбэк не обрабатывается обработчиком.
+     */
     @Override
     public EditMessageText handle(Update update) {
         CallbackQuery callbackQuery = update.callbackQuery();
--- telegram-bot/src/main/java/pro/sky/telegrambot/handler/message/StartMessageHandler.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/handler/message/StartMessageHandler.java
@@ -6,12 +6,30 @@ import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendMessage;
 
+/**
+ * Обработчик для сообщений, содержащих команду /start.
+ * Возвращает сообщение с клавиатурой выбора приюта для кошек или собак.
+ */
 public class StartMessageHandler implements MessageChainHandler {
+
+    /**
+     * Проверяет, соответствует ли сообщение команде /start.
+     *
+     * @param update Обновление от Telegram API
+     * @return true, если сообщение содержит команду /start, иначе false.
+     */
     @Override
     public boolean check(Update update) {
         Message message = update.message();
         return message != null && message.text().contains("/start");
     }
+
+    /**
+     * Обрабатывает сообщение с командой /start и возвращает сообщение с клавиатурой выбора приюта.
+     *
+     * @param update Обновление от Telegram API
+     * @return SendMessage объект с сообщением и клавиатурой для отправки обратно в Telegram.
+     */
     @Override
     public SendMessage handle(Update update) {
         Message message = update.message();
